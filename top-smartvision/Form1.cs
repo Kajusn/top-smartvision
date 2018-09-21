c2dfe7e30fdfd33e07e68ebc4b692566873eac22
Kajusn <43174440+Kajusn@users.noreply.github.com>
2018-09-21 19:23:41 +0300

    Added basic GUI
    Can upload an image, which will be displayed in the form

--- top-smartvision/Form1.cs
+++ top-smartvision/Form1.cs
@@ -19,7 +19,57 @@ namespace top_smartvision
         {
             InitializeComponent();
             Recognition test = new Recognition();
-            test.Recognizer();
+            //test.Recognizer();
         }
+
+        #region Upload image
+
+        /// <summary>
+        /// Browsw system for image to upload and display
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UploadImgButton_Click(object sender, EventArgs e)
+        {
+            // Open file dialog
+            OpenFileDialog open = new OpenFileDialog();
+            
+            // Filters the types of files that can be opened
+            open.Filter = "*.jpg; *jpeg; *.bmp; *.png|*.jpg; *jpeg; *.bmp; *.png";
+            if (open.ShowDialog() != DialogResult.OK)
+                return;
+            
+            // Loads and displays image
+            ImgBox.LoadAsync (open.FileName);
+
+            // Displays image path
+            ImageNameLabel.Text = open.FileName;
+        }
+
+        /// <summary>
+        /// Hides upload img prompt when img loads
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ImgBox_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            // Hides "Upload image" prompt
+            UploadImgLabel.Visible = false;
+            UploadImgButton.Text = "Upload another image";
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Runs image recognizer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AnalyzeButton_Click(object sender, EventArgs e)
+        {
+            // TO-DO: Runs Recognizer with opened image
+        }
+
+        
     }
 }
