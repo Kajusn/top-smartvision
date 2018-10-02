431c2df5a3a8093af0f9d883861c6f31a2631aa3
Kajusn <43174440+Kajusn@users.noreply.github.com>
2018-10-02 13:26:20 +0300

    Login screen (alpha)

---
+++ top-smartvision/Form2.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace top_smartvision
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.UsernameText = new System.Windows.Forms.TextBox();
+            this.PasswordText = new System.Windows.Forms.TextBox();
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.RegsiterBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.NameLabel.Location = new System.Drawing.Point(12, 9);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(310, 23);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Smart Vision";
+            this.NameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.UsernameText, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.PasswordText, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(17, 138);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(305, 100);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // UsernameText
+            // 
+            this.UsernameText.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.UsernameText.Location = new System.Drawing.Point(77, 15);
+            this.UsernameText.Name = "UsernameText";
+            this.UsernameText.Size = new System.Drawing.Size(150, 20);
+            this.UsernameText.TabIndex = 0;
+            this.UsernameText.Text = "Username";
+            this.UsernameText.Enter += new System.EventHandler(this.UsernameText_Enter);
+            this.UsernameText.Leave += new System.EventHandler(this.UsernameText_Leave);
+            // 
+            // PasswordText
+            // 
+            this.PasswordText.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PasswordText.Location = new System.Drawing.Point(77, 65);
+            this.PasswordText.Name = "PasswordText";
+            this.PasswordText.Size = new System.Drawing.Size(150, 20);
+            this.PasswordText.TabIndex = 1;
+            this.PasswordText.Text = "Password";
+            this.PasswordText.Enter += new System.EventHandler(this.PasswordText_Enter);
+            this.PasswordText.Leave += new System.EventHandler(this.PasswordText_Leave);
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LoginLabel.Location = new System.Drawing.Point(17, 74);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(305, 23);
+            this.LoginLabel.TabIndex = 2;
+            this.LoginLabel.Text = "Login to your account";
+            this.LoginLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.LoginBtn, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.RegsiterBtn, 0, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(66, 256);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(200, 102);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LoginBtn.Location = new System.Drawing.Point(3, 3);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(194, 29);
+            this.LoginBtn.TabIndex = 0;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            // 
+            // RegsiterBtn
+            // 
+            this.RegsiterBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RegsiterBtn.Location = new System.Drawing.Point(3, 38);
+            this.RegsiterBtn.Name = "RegsiterBtn";
+            this.RegsiterBtn.Size = new System.Drawing.Size(194, 29);
+            this.RegsiterBtn.TabIndex = 1;
+            this.RegsiterBtn.Text = "Register";
+            this.RegsiterBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 421);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.LoginLabel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.NameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox UsernameText;
+        private System.Windows.Forms.TextBox PasswordText;
+        private System.Windows.Forms.Label LoginLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.Button RegsiterBtn;
+    }
+}
