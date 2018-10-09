3926a9012db31e3da7d293313f8bed5ad9706451
Deividas Šimkus <deividas.simkus@mif.stud.vu.lt>
2018-10-09 15:36:36 +0300

    created Recognition testing class

---
+++ UnitTestProject/RecognitionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using top_smartvision.recognition;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class RecognitionTest
+    {
+        private readonly Recognition _recognition;
+
+        public RecognitionTest()
+        {
+            _recognition = new Recognition();
+        }
+
+        [TestMethod]
+        public void Skeletonize()
+        {
+            string i = "1";
+            Bitmap bitmap = new Bitmap(i);
+            var result = _recognition.Skeletonize(bitmap);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
