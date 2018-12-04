f7da5f795c4d8b1b466cd334590a850e12b5ccb7
Deividas Šimkus <deividas.simkus@mif.stud.vu.lt>
2018-12-04 11:06:15 +0200

    camera permission requests

--- LostAndFound/LostAndFound/FoundController.cs
+++ LostAndFound/LostAndFound/FoundController.cs
@@ -1,4 +1,5 @@
 ﻿using LostAndFound.Models;
+using Plugin.Media;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +9,33 @@ namespace LostAndFound
 {
     class FoundController
     {
-        public static void PickPic(ContentPage instance)
+        public async static void PickPic(ContentPage instance)
         {
+            await CrossMedia.Current.Initialize();
 
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                instance.DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await instance.DisplayAlert("File Location", file.Path, "OK");
+
+            Image image = new Image();
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
         }
 
         public static void Save(IFoundModel model, ContentPage instance)
