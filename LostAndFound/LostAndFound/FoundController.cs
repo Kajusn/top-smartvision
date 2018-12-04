da87a418af02b363dbfa3af2589e92408f230775
Deividas Šimkus <deividas.simkus@mif.stud.vu.lt>
2018-12-04 11:50:31 +0200

    catch exception if camera permissions not granted

--- LostAndFound/LostAndFound/FoundController.cs
+++ LostAndFound/LostAndFound/FoundController.cs
@@ -1,5 +1,6 @@
 ﻿using LostAndFound.Models;
 using Plugin.Media;
+using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,31 +12,41 @@ namespace LostAndFound
     {
         public async static void PickPic(ContentPage instance)
         {
+            var uniqueFileName = $@"{Guid.NewGuid()}.jpg";
+
             await CrossMedia.Current.Initialize();
 
-            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            try
             {
-                instance.DisplayAlert("No Camera", ":( No camera available.", "OK");
-                return;
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await instance.DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = uniqueFileName
+                });
+
+                if (file == null)
+                    return;
+
+                await instance.DisplayAlert("File Location", file.Path, "OK");
+
+                Image image = new Image();
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
             }
-
-            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            catch(MediaPermissionException)
             {
-                Directory = "Sample",
-                Name = "test.jpg"
-            });
-
-            if (file == null)
-                return;
-
-            await instance.DisplayAlert("File Location", file.Path, "OK");
-
-            Image image = new Image();
-            image.Source = ImageSource.FromStream(() =>
-            {
-                var stream = file.GetStream();
-                return stream;
-            });
+                await instance.DisplayAlert("No Camera Permissions", ":( No camera permissions granted. Please check them in App info > Permissions (:", "OK");
+            }
+            
         }
 
         public static void Save(IFoundModel model, ContentPage instance)
